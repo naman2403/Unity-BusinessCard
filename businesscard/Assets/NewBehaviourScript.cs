35a28f1c228c616878563b94b5ae68545d2287b6
Naman Khatwani <54111841+naman2403@users.noreply.github.com>
2021-01-16 23:46:50 +0530

    First Commit
    Uploading Unity project on Github.

---
+++ businesscard/Assets/NewBehaviourScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    private bool dirRight = true;
+    public float speed = 2.0f;
+
+   
+    void Update()
+    {
+        if (dirRight)
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+       /* else
+            transform.Translate(-Vector2.right * speed * Time.deltaTime);
+            */
+        if (transform.position.x >= 2.0f)
+        {
+            dirRight = false;
+        }
+
+        /*if (transform.position.x <= -4)
+        {
+            dirRight = true;
+        }*/
+    }
+}
